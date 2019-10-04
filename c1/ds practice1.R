1a661e98aa4501b9cba5da9dd9001cc6a04cd6cb
geetha2467 <geetha2467@gmail.com>
2019-10-04 16:56:42 +0530

    4/10/19

--- c1/ds practice1.R
+++ c1/ds practice1.R
@@ -78,7 +78,126 @@ df1
 df1$empsec = c(01,02,01,04,05)
 df1
 
+#dataframe-----------
 
+dat = data.frame(name= c("anu","mani","lak","buji","raji"),salary = c(100,200,500,800,900),job= c("artist","dataanalytics","fashiondesigner","CA","illustrator"),stringsAsFactors = FALSE)
+dat
+
+data.frame(dat$name,dat$salary)
+data.frame(dat$salary,dat$job)
+
+#add column
+
+dat$maritalstatus = c("no","yes","no","no","yes")
+
+dat
+
+#add row
+
+dat1 = data.frame(name= c("manu","anil","karan","pabi","ravi"),salary = c(350,250,550,450,750),job= c("teacher","developer","actor","analyst","graphicdesigner"),stringsAsFactors = FALSE)
+dat1
+
+dat1$maritalstatus = c("yes","no","yes","no","yes")
+dat1
+
+#add row
+
+finaldat = rbind(dat,dat1)
+finaldat
+
+finaldat[,1:3]
+finaldat[1:5,]
+finaldat[,]
+finaldat[c(1:5),c(1,3,2)]
+
+rn= c("r1","r2","r3","r4","r5")
+cn= paste("C",1:2,sep="")
+to= matrix(c(2:11),nrow = 5,byrow = TRUE,dimnames = list(rn,cn))
+to
+
+#list--------------------------
+lis = list(name=c(1:10),matrix= matrix(c(1:8),nrow = 4,byrow=TRUE,dimnames = list(rn=c("r1","r2","r3","r4"),cn=c("c1","c2"))),color=c("red","blue","grrrey"))
+lis
+
+#array----------------(multidimensional)
+
+array1 = array(c(1:9),dim=c(2,2,3))
+array1
+
+
+dafra = data.frame(name=c("anil","pabi","kani"),std= c("1st","4th","7th"),rank=c("1","2","3"),stringsAsFactors = FALSE)
+dafra
+
+str(dafra)
+summary(dafra)
+dafra[,2]
+
+dafra$favchocolate = c("cadbury","kitkat","diarymilk")
+dafra
+
+dafra1= data.frame(name=c("kanil","mpabi","jikani"),std= c("2nd","5th","8th"),rank=c("5","8","9"),stringsAsFactors = FALSE)
+dafra1
+
+dataframe123= rbind(dafra,dafra1)
+dataframe123
+
+dataframe123[1:5,]
+
+
+#loop-----------------------
+
+A= c(1:10)
+for(i in A)
+{
+  print(i)
+}
+
+AB = c(1:150)
+cnt = 3
+while(cnt<20)
+{
+  print(cnt)
+  cnt = cnt+5
+}
+
+B = c(1:80)
+for(i in B)
+{
+  print(i)
+}
+
+BA = c(5:150)
+i = 5
+while(i<50)
+{
+  print(i)
+  i=i+5
+}
+
+#data import-------------
+
+getwd()
+setwd("C:\\Users\\Admin\\Desktop\\R")
+
+tt= read.csv(file.choose())
+
+AST = read.csv("abc.csv")
+
+install.packages("readr")
+library(readr)
+
+table = read.table(file.choose())
+table1 = read.table("abc.txt",sep = "",header=TRUE)
+install.packages("readxl")
+library(readxl)
+excel_sheets ("abc.xlsx")
+read_excel("abc.xlsx",sheet = 1)
+read_excel("abc.xlsx",sheet=2)
+read_excel("abc.xlsx",shhet=3)
+
+#ExportData---------------------------
+
+ZX = write.csv(AST,"C:/Users/Admin/Desktop/R/myexportdata.csv")
 
 
 
